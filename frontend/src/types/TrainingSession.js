e0947331064da761c892d5c0d2ed668d8941930c
andersonfariassantos02-beep <anderson.farias.santos.02@gmail.com>
2026-06-10 10:40:29 -0400

    Criar TrainingSession.js

---
+++ frontend/src/types/TrainingSession.js
@@ -0,0 +1,17 @@
+export const TrainingSession = {
+  id: "",
+
+  userId: "",
+
+  workoutId: "",
+
+  date: "",
+
+  durationMinutes: 0,
+
+  perceivedEffort: 0,
+
+  completed: false,
+
+  observations: "",
+};
