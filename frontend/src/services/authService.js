40a75220ec56916d5982b5de67ff6b3b6d068abf
andersonfariassantos02-beep <anderson.farias.santos.02@gmail.com>
2026-06-10 09:57:02 -0400

    Criar authService.js

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,27 @@
+import { supabase } from "../lib/supabase";
+
+export async function login(email, password) {
+  return await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+}
+
+export async function register(email, password) {
+  return await supabase.auth.signUp({
+    email,
+    password,
+  });
+}
+
+export async function logout() {
+  return await supabase.auth.signOut();
+}
+
+export async function getCurrentUser() {
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  return user;
+}
