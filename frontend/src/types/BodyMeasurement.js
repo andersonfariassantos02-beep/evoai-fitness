441c425d3fe74013cd009e18abc3496fb35d66ce
andersonfariassantos02-beep <anderson.farias.santos.02@gmail.com>
2026-06-10 10:38:53 -0400

    Criar BodyMeasurement.js

---
+++ frontend/src/types/BodyMeasurement.js
@@ -0,0 +1,33 @@
+export const BodyMeasurement = {
+  id: "",
+
+  userId: "",
+
+  date: "",
+
+  neck: 0,
+
+  chest: 0,
+
+  waist: 0,
+
+  abdomen: 0,
+
+  hips: 0,
+
+  rightArm: 0,
+
+  leftArm: 0,
+
+  rightForearm: 0,
+
+  leftForearm: 0,
+
+  rightThigh: 0,
+
+  leftThigh: 0,
+
+  rightCalf: 0,
+
+  leftCalf: 0,
+};
