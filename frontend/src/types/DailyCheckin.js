f1aece954fa02e463201099b908a02a0417027f1
andersonfariassantos02-beep <anderson.farias.santos.02@gmail.com>
2026-06-10 10:37:35 -0400

    Criar DailyCheckin.js

---
+++ frontend/src/types/DailyCheckin.js
@@ -0,0 +1,19 @@
+export const DailyCheckin = {
+  id: "",
+
+  userId: "",
+
+  date: "",
+
+  weight: 0,
+
+  water: 0,
+
+  sleepHours: 0,
+
+  calories: 0,
+
+  workoutDone: false,
+
+  notes: "",
+};
