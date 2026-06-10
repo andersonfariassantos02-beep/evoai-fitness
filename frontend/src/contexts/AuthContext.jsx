4cea62b4755608f7224692ffdd60461471bde1f9
andersonfariassantos02-beep <anderson.farias.santos.02@gmail.com>
2026-06-10 01:58:28 -0400

    Create AuthContext.jsx

---
+++ frontend/src/contexts/AuthContext.jsx
@@ -0,0 +1,15 @@
+import { createContext, useContext } from "react";
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  return (
+    <AuthContext.Provider value={{}}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
