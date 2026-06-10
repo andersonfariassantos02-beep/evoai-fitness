53c474feb1a21bfb5090d50620a7a94d72ace3a1
andersonfariassantos02-beep <anderson.farias.santos.02@gmail.com>
2026-06-10 01:59:17 -0400

    Create LoginPage.jsx

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,23 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <h2>Login EvoAI Fitness</h2>
+
+      <input
+        type="email"
+        placeholder="Seu email"
+      />
+
+      <br />
+
+      <input
+        type="password"
+        placeholder="Sua senha"
+      />
+
+      <br />
+
+      <button>Entrar</button>
+    </div>
+  );
+}
